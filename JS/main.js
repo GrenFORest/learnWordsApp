b543df9d74935040018a282fcaec1cc538e6fe5e
GrenFORest <agurza23@yahoo.com>
2023-11-23 23:59:16 +0300

    123123

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,10 @@
 import {dictionaryWords,temp2} from './dict.js';
 
-let words = ['Рыба', 'Кот', 'Мышка', 'Дом'];
+let words = ['Утро', '12321', 'День', 'Завод', 'Лом', 'Рак', 'Енот', 'Соболь','Рыба', 'Кот', 'Мышка', 'Дом'];
 
 
 let word = words[Math.floor(Math.random() * words.length)] + "!";
 
-
+document.getElementById('word').innerHTML = word;
 
 console.log(dictionaryWords,temp2,word);
